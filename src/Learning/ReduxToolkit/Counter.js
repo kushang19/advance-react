dd9047da9feff4a71ee3a9a6d6f6fd80580498c7
Kushang Vijay Gharat <kushang81@gmail.com>
2025-08-25 23:02:57 +0530

    js code

--- src/Learning/ReduxToolkit/Counter.js
+++ src/Learning/ReduxToolkit/Counter.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { dec, inc, incBy } from './CounterSlice';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { dec, inc, incBy } from "./CounterSlice";
 
 const Counter = () => {
-
-  const count = useSelector(state => state.counter.value)
-  
+  const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
+  var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for(let j = i+1; j < nums.length; j++){
+            if(nums[i] === nums[j]){
+                return [i, j]
+            }
+        }
+    }
+    return -1;
+  };
+
+  const result = twoSum([3,5,5,11,90,10000,67, 98],101); // [0, 7]
+  console.log("Answer ", result);
 
   return (
     <>
@@ -15,13 +26,13 @@ const Counter = () => {
         hello world
         <h1>{count}</h1>
         <div>
-            <button onClick={() => dispatch(inc())} >+1</button>
-            <button onClick={() => dispatch(dec())} >-1</button>
-            <button onClick={() => dispatch(incBy(5))} >+5</button>
+          <button onClick={() => dispatch(inc())}>+1</button>
+          <button onClick={() => dispatch(dec())}>-1</button>
+          <button onClick={() => dispatch(incBy(5))}>+5</button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
